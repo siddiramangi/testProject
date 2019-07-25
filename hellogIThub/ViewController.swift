7b69c6063e8a399fdac0f2e3e4ecb071469ecdc0
SIDDI RAMANJANEYA REDDY <siddiramangi@achiever-117.local>
2019-07-25 16:01:38 +0530

    hi sudhirrrrr take a review on itpand pass to me once u r done....

--- hellogIThub/ViewController.swift
+++ hellogIThub/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+      // hello master how are you..uppal bal ti tak famous
+        
+        print("siddi hellooooooo")
     }
 
 
---
+++ hellogIThub/helloooo.swift
@@ -0,0 +1,30 @@
+//
+//  helloooo.swift
+//  hellogIThub
+//
+//  Created by SIDDI RAMANJANEYA REDDY on 7/25/19.
+//  Copyright © 2019 SIDDI RAMANJANEYA REDDY. All rights reserved.
+//
+
+import UIKit
+
+class helloooo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       print("hiiiiiiiiiiiiiii")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
